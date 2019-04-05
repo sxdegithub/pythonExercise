25f470ce8b9f2ce9782fa88cd43a6cec664a2eee
sxdegithub <1556499949@qq.com>
2019-04-05 20:22:15 +0800

    del .idea

--- pythonExercise/test.py
+++ pythonExercise/test.py
@@ -1,3 +1,21 @@
 # coding=utf-8
-key = lambda item: item[1]
-print(key(2))
+# key = lambda item: item[1]
+# print(key(2))
+
+a = ("jpeg", "jpg")
+b = ("jpeg", "jpg", "png")
+i = "png"
+
+if i not in a:
+    print("i not in a ")
+else:
+    print("i is in a")
+
+if i not in b:
+    print("i not in b ")
+else:
+    print("i is in b")
+
+
+str = '123132231213321312==321312213231123132'
+print(str.strip('123'))
