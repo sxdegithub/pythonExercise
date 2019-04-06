4fcaf19b6647d8c4834b0002a52c722ef17a622e
sxdegithub <1556499949@qq.com>
2019-04-06 14:40:22 +0800

    start solve 008

--- pythonExercise/py006/py006.py
+++ pythonExercise/py006/py006.py
@@ -7,7 +7,33 @@
 # 统计单个文件中单词出现频率
 # 将所有文件中单词出现频率合并
 
+import os
+import pathlib
+import re
 
+diary_path = "C:\\Users\\ssau\\PycharmProjects\\pythonExercise\\py006"
+# diary_files = os.listdir(diary_path)
 
+# 过滤出txt文
+# 件来
+path = pathlib.Path(diary_path)
+diary_files = path.glob("*.txt")
 
-
+mapping = dict()
+for i in diary_files:
+    # print(i)
+    print(os.path.split(i)[-1])
+    with open(i, encoding="utf-8") as f:
+        data = f.read()
+        # 匹配所有word
+        words = re.findall("\w+", data)
+        words = [word.lower() for word in words]
+        # 将word数量存入字典
+        for word in words:
+            mapping[word] = mapping.get(word, 0) + 1
+        # print(mapping)
+        # print(word)
+# 对字典排序
+# python内置的Counter可以实现,未尝试过
+mapping = sorted(mapping.items(), key=lambda item: item[-1], reverse=True)
+print(mapping[:5])
